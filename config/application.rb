4f73a504f94087457ab4e810287c255bfe415937
Rodrigo-Vargas <rodrigovargas123@gmail.com>
2017-05-07 20:35:31 -0300

    Add support to pt language. Added jquery

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module RodrigoVargas
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    I18n.config.available_locales = [:pt, :en]
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
