4db3e048f701e5a0d64c505a14c7e2b013315b88
Rodrigo-Vargas <rodrigovargas123@gmail.com>
2017-12-30 17:17:13 +0000

    Fixes on layout

--- assets/js/timeline.js
+++ assets/js/timeline.js
@@ -3,6 +3,11 @@ Timeline = function (options) {
       this.selector = options.selector;
       this.parent = options.parent;
 
+      this.processItems();
+   }
+
+   this.processItems = function(){
+      console.log('processItems');
       var items = document.querySelectorAll(this.selector);
 
       var margin = 30;
@@ -12,32 +17,47 @@ Timeline = function (options) {
       var parentHeight = 0;
       var top = 0;
 
-      for(var x = 0; x < items.length; x++)
-      {
-         var style = "position: absolute;";
-         style += "width: 50%;";
+      var viewportWidth = Math.min(document.documentElement.clientWidth, window.innerWidth || 0);
+      
+      var width = 50;
+
+      if (viewportWidth < 768)
+         width = 100;
+
+      for (var x = 0; x < items.length; x++) {
+         var style = "";
+
+         if (viewportWidth < 768)
+         {
+            items[x].style = "";
+            continue;
+         }            
+
+         style += "width: " + width + "%;";
          style += "top: " + top + "px;";
 
-         if (x % 2 == 0)
-         {
+         if (x % 2 == 0) {
             style += "top: " + firstColumnHeight + "px;";
             items[x].classList.add("odd");
             style += "left: 0";
             firstColumnHeight += items[x].offsetHeight + margin;
-         }            
-         else
-         {
+         }
+         else {
             style += "top: " + secondColumnHeight + "px;";
             items[x].classList.add("couple");
             style += "right: 0";
             secondColumnHeight += items[x].offsetHeight + margin;
          }
-         
+
          items[x].style = style;
       }
 
-      document.querySelector(this.parent).style = "height: " + secondColumnHeight + "px;";
+      if (viewportWidth < 750)
+         document.querySelector(this.parent).style = "height: auto;";
+      else
+         document.querySelector(this.parent).style = "height: " + secondColumnHeight + "px;";
    }
 
    this.init(options);
+   window.addEventListener('resize', this.processItems)
 }
