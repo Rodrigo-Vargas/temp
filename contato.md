fb2c6689cddf75fb23a51180ab2c1c1cd23ad3ed
Ubuntu <rodrigo-vargas>
2018-11-04 12:40:28 -0200

    Removed unused scss files. Create contact page. Builded header

---
+++ contato.md
@@ -0,0 +1,46 @@
+--- 
+   layout: default
+   permalink: /contato/
+---
+
+<div id="content">
+   <section id="contact" class="contact ">
+      <div class="inner">
+         <div class="container">
+            <div class="title">
+               <h2 class="section-title">Contact</h2>
+               <span class="section-excerpt">
+                  Say hello!
+               </span>
+            </div>
+            
+            <div class="row">
+               <div class="col-12">
+                  <div class="box-with-shadow">
+                     <form action="">
+                        <div class="form-group">
+                           <input required type="text" class="form-control" name="name" placeholder="Name">
+                        </div>
+                     
+                        <div class="form-group">
+                           <input   required type="text" 
+                                    class="form-control" name="email" 
+                                    placeholder="Email" 
+                                    data-inputmask="'alias': 'email', 'clearIncomplete': true">
+                        </div>
+                     
+                        <div class="form-group">
+                           <textarea required type="text" class="form-control" name="body" rows="6" placeholder="Message"></textarea>
+                        </div>
+                     
+                        <div class="form-group">
+                           <button type="submit" href="#" class="btn btn-primary">Send</button>
+                        </div>
+                     </form>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </section>
+</div>
--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
-var env           = require('minimist')(process.argv.slice(2));
+var env           = require("minimist")(process.argv.slice(2));
 var gulp          =  require("gulp");
-var plumber       = require('gulp-plumber');
-var browserSync   = require('browser-sync');
-var cp            = require('child_process');
-var concat        = require('gulp-concat');
-var uglify        = require('gulp-uglify');
-var sass          = require('gulp-sass');
+var plumber       = require("gulp-plumber");
+var browserSync   = require("browser-sync");
+var cp            = require("child_process");
+var concat        = require("gulp-concat");
+var uglify        = require("gulp-uglify");
+var sass          = require("gulp-sass");
 
 
 
@@ -13,61 +13,62 @@ var messages = {
    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
 };
 
-gulp.task('jekyll-build', function (done) {
+gulp.task("jekyll-build", function (done) {
    browserSync.notify(messages.jekyllBuild);
-   return cp.spawn('bundle', ['exec', 'jekyll build'], { stdio: 'inherit' })
-      .on('close', done);
+   return cp.spawn("bundle", ["exec", "jekyll build"], { stdio: "inherit" })
+      .on("close", done);
 });
 
-gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+gulp.task("jekyll-rebuild", ["jekyll-build"], function () {
    browserSync.reload();
 });
 
 
-gulp.task('browser-sync', ['jekyll-build'], function () {
+gulp.task("browser-sync", ["jekyll-build"], function () {
    browserSync({
       server: {
-         baseDir: '_site'
+         baseDir: "_site"
       }
    });
 });
 
-gulp.task('js', function () {
-   return gulp.src((env.p) ? 'src/js/**/*.js' : ['src/js/**/*.js', '!src/js/analytics.js'])
+gulp.task("js", function () {
+   return gulp.src((env.p) ? "src/js/**/*.js" : ["src/js/**/*.js", "!src/js/analytics.js"])
       //.pipe(plumber())
-      //.pipe(concat('main.js'))
+      //.pipe(concat("main.js"))
       //.pipe(uglify())
-      .pipe(gulp.dest('assets/js/'));
+      .pipe(gulp.dest("assets/js/"));
 });
 
-gulp.task('sass', function () {
+gulp.task("sass", function () {
    var sassDevOptions = {
-      outputStyle: 'expanded'
+      outputStyle: "expanded"
    }
 
    /* Compile inline styles and put on includes*/
-   gulp.src(['./src/sass/inline-*.scss'])
-      .pipe(sass(sassDevOptions).on('error', sass.logError))
-      .pipe(gulp.dest('_includes/'));
+   gulp.src(["./src/sass/inline-*.scss"])
+      .pipe(sass(sassDevOptions).on("error", sass.logError))
+      .pipe(gulp.dest("_includes/"));
 
-   return gulp.src(['./src/sass/*.scss', '!src/sass/inline-*.scss'])
-      .pipe(sass(sassDevOptions).on('error', sass.logError))
-      .pipe(gulp.dest('assets/css/'));
+   return gulp.src(["./src/sass/*.scss", "!src/sass/inline-*.scss"])
+      .pipe(sass(sassDevOptions).on("error", sass.logError))
+      .pipe(gulp.dest("assets/css/"));
 });
 
-gulp.task('watch', function () {
-   gulp.watch('src/sass/**/*.scss', ['sass']);
-   gulp.watch('src/js/**/*.js', ['js', 'jekyll-rebuild']);
+gulp.task("watch", function () {
+   gulp.watch("src/sass/**/*.scss", ["sass"]);
+   gulp.watch("src/js/**/*.js", ["js", "jekyll-rebuild"]);
    gulp.watch(
                   [
-                     //'**/*.html', 
-                     'index.html', 
-                     '_includes/*.html', 
-                     '_layouts/*.html', 
-                     //'_posts/*'
+                     "**/*.md",
+                     "index.html", 
+                     "_includes/*.html", 
+                     "_layouts/*.html", 
+                     //"_posts/*",
+                     "!_site/*/**}"
                   ],
-                  ['jekyll-rebuild']
+                  ["jekyll-rebuild"]
             );
 });
 
-gulp.task('default', ['js', 'sass', 'browser-sync', 'watch']);
+gulp.task("default", ["js", "sass", "browser-sync", "watch"]);
