126fe7d7bcfd5b1366b56b99188032e14e338059
Rodrigo-Vargas <rodrigo.vargas@conectt.com.br>
2021-03-07 20:55:28 -0300

    Added style to header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,35 +1,44 @@
 import React from 'react';
 import headerPropType from './propTypes';
 
-import { Logo, LogoText } from './styles';
+import {
+  HeaderContainer,
+  HeaderGroup,
+  HeaderWrapper,
+  Logo,
+  LogoText,
+  NavGroup,
+  NavWrapper,
+  NavItem,
+  NavItemText,
+} from './styles';
 
 const Header = ({ items }) => (
-  <header className="z-10 fixed w-full bg-gray-50 transition-all duration-500" data-collapse="scroll" data-collapse-trigger-height="100">
-    <div className="py-5 border-b border-gray-200 dark:border-gray-800   dark:bg-gray-900 z-10">
-      <div className="container mx-auto">
-        <div className="flex items-center">
+  <HeaderWrapper data-collapse="scroll" data-collapse-trigger-height="100">
+    <HeaderGroup>
+      <HeaderContainer>
+        <Logo>
+          <LogoText href="/">RV</LogoText>
+        </Logo>
 
-          <div className="w-3/4 md:w-1/4">
-            <Logo>
-              <LogoText href="/">RV</LogoText>
-            </Logo>
-          </div>
-
-          <nav id="top-nav" className="nav w-64 md:w-3/4 hidden md:flex flex-col md:flex-row items-center text-center justify-center md:justify-end z-20">
-            <ul className="flex flex-col md:flex-row items-center justify-end">
-              { items?.map((map) => (
-                <li key={map.title} className="pl-5">
-                  <a className="nav-link block {{cssClass}}" href={map.href} target="{{target}}">
-                    <span className="text-gray-500">{map.title}</span>
-                  </a>
-                </li>
-              )) }
-            </ul>
-          </nav>
-        </div>
-      </div>
-    </div>
-  </header>
+        <NavGroup>
+          <NavWrapper>
+            {items?.map((map) => (
+              <li key={map.title} className="pl-5">
+                <NavItem
+                  className="{{cssClass}}"
+                  href={map.href}
+                  target="{{target}}"
+                >
+                  <NavItemText>{map.title}</NavItemText>
+                </NavItem>
+              </li>
+            ))}
+          </NavWrapper>
+        </NavGroup>
+      </HeaderContainer>
+    </HeaderGroup>
+  </HeaderWrapper>
 );
 
 Header.defaultProps = {
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -38,15 +38,94 @@ export const Logo = styled.div`
 export const LogoText = styled.a`
   ${({ theme }) => css`
     align-items: center;
-    color: ${theme.colors.primary500};
+    color: ${theme.colors.primary700};
     display: flex;
     height: 100%;
     justify-content: center;
     position: absolute;
     text-align: center;
+    text-decoration: none;
     z-index: 10;
     width: 100%;
   `}
 `;
 
-export const Another = styled.div``;
+export const NavGroup = styled.nav`
+  display: flex;
+  margin-left: auto;
+`;
+
+export const NavWrapper = styled.ul`
+  align-items: center;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
+
+export const NavItem = styled.a`
+  display: block;
+  padding-left: 1rem;
+`;
+
+export const NavItemText = styled.span`
+  ${({ theme }) => css`
+    color: ${theme.colors.gray500};
+  `}
+`;
+
+export const HeaderWrapper = styled.header`
+  ${({ theme }) => css`
+    background-color: ${theme.colors.gray50};
+    position: fixed;
+    width: 100%;
+    transition: all 0.5s;
+    z-index: 10;
+  `}
+`;
+
+export const HeaderGroup = styled.div`
+  ${({ theme }) => css`
+    border-color: ${theme.colors.gray200};
+    border-style: solid;
+    padding-top: 1.25rem;
+    padding-bottom: 1.25rem;
+    border-bottom-width: 1px;
+    z-index: 10;
+  `}
+`;
+
+export const Container = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  padding-right: 1rem;
+  padding-left: 1rem;
+  width: 100%;
+
+  @media (min-width: 640px) {
+    max-width: 640px;
+    padding-right: 2rem;
+    padding-left: 2rem;
+  }
+
+  @media (min-width: 768px) {
+    max-width: 768px;
+    padding-right: 4rem;
+    padding-left: 4rem;
+  }
+
+  @media (min-width: 1024px) {
+    max-width: 1024px;
+    padding-right: 5rem;
+    padding-left: 5rem;
+  }
+
+  @media (min-width: 1280px) {
+    max-width: 1280px;
+    padding-right: 7rem;
+    padding-left: 7rem;
+  }
+`;
+
+export const HeaderContainer = styled(Container)`
+  display: flex;
+`;
