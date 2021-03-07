3adb9fe8d59d3a5dcdd3e41cbf08c8183c95447a
Rodrigo-Vargas <rodrigo.vargas@conectt.com.br>
2021-03-07 18:25:49 -0300

    Added gatsby global layout properties

--- src/html.jsx
+++ src/html.jsx
@@ -1,26 +1,39 @@
+/* eslint-disable react/forbid-prop-types */
 /* eslint-disable react/no-danger */
 import React from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import PropTypes from 'prop-types';
 
-const HTML = ({ body }) => (
+const HTML = ({
+  body, headComponents, preBodyComponents, postBodyComponents,
+}) => (
   <html lang="EN">
+    <head />
+    {headComponents}
     <body>
+      {preBodyComponents}
       <div
         key="body"
         id="___gatsby"
         dangerouslySetInnerHTML={{ __html: body }}
       />
+      {postBodyComponents}
     </body>
   </html>
 );
 
 HTML.defaultProps = {
   body: '',
+  headComponents: [],
+  preBodyComponents: [],
+  postBodyComponents: [],
 };
 
 HTML.propTypes = {
+  headComponents: PropTypes.array,
+  preBodyComponents: PropTypes.array,
   body: PropTypes.string,
+  postBodyComponents: PropTypes.array,
 };
 
 export default HTML;
