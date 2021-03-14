8ee7b31e59b4175ab77ddf0c146d682113b7e794
Rodrigo-Vargas <rodrigo.vargas@conectt.com.br>
2021-03-14 17:16:55 -0300

    Added test coverage to project

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,19 @@ module.exports = {
   transform: {
     '^.+\\.jsx?$': '<rootDir>/jest-preprocess.js',
   },
+  testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.js'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.js(x)?',
+    '!src/pages/**/*.js(x)',
+    '!src/html.jsx',
+    '!src/styles/**/*.js',
+  ],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  coverageReporters: [
+    'text-summary',
+    'lcov',
+  ],
 };
