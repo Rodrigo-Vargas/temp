36108b07fcde1e88174f3fca4283022ec3d9d39e
Rodrigo Vargas <rodrigovargas123@gmail.com>
2021-10-17 19:28:17 -0300

    Minor fixes on fonts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -14,16 +14,15 @@ const GlobalStyles: GlobalStyleComponent<
     font-weight: 300;
     font-display: swap;
     src: local('Poppins Light'), local('Poppins-Light'),
-        url('/fonts/poppins-v12-latin-300.woff2') format('woff2');
+        url('/fonts/poppins-v15-latin-300.woff2') format('woff2');
   }
 
   @font-face {
     font-family: 'Poppins';
     font-style: normal;
-    font-weight: 400;
+    font-weight: 500;
     font-display: swap;
-    src: local('Poppins Regular'), local('Poppins-Regular'),
-        url('/fonts/poppins-v12-latin-regular.woff2') format('woff2');
+    src: url('/fonts/poppins-v15-latin-regular.woff2') format('woff2');
   }
 
   @font-face {
@@ -32,10 +31,40 @@ const GlobalStyles: GlobalStyleComponent<
     font-weight: 600;
     font-display: swap;
     src: local('Poppins SemiBold'), local('Poppins-SemiBold'),
-        url('/fonts/poppins-v12-latin-600.woff2') format('woff2');
+        url('/fonts/poppins-v15-latin-600.woff2') format('woff2');
+  }
+
+  *, *:before, *:after {
+    border-width: 0;
+    box-sizing: border-box;
+  }
+
+  body, h1, h2, h3, h4, h5, h6, p, ol, ul {
+    margin: 0;
+    padding: 0;
+    font-weight: normal;
+  }
+
+  ol, ul {
+    list-style: none;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+
+  .w-50 {
+    width: 50%;
   }
 
   ${({ theme }) => css`
+    html {
+      background-color: ${theme.colors.gray100};
+      box-sizing: border-box;
+      font-size: 16px;
+    }
+
     body {
       font-family: ${theme.font.family};
     }
